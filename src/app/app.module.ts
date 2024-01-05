e51d68db4592cf3d84033dbcb314640274c9dbc5
Imrane Arabah <59772524+arabahimrane@users.noreply.github.com>
2024-01-04 22:45:02 -0500

    Premier commit avec du code initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { MainComponent } from './component/main/main.component';
+import { AboutComponent } from './component/about/about.component';
+import { CserviceComponent } from './component/cservice/cservice.component';
+import { PresentationComponent } from './component/presentation/presentation.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    PresentationComponent,
+    CserviceComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
