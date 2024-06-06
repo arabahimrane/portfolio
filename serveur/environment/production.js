812cd24a9604f09c2b1f82225fa8474f8fceb23e
Imrane Arabah <59772524+arabahimrane@users.noreply.github.com>
2024-06-06 19:36:48 -0400

    production

---
+++ serveur/environment/production.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+module.exports = {
+    dbUrl: "mongodb+srv://alpha:12345@realshow.iaogxvs.mongodb.net/portfolio?retryWrites=true&w=majority",
+    portHttp: 80,
+    portHttps: 443,
+};
