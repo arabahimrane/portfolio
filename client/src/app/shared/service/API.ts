88d9d046b0971834016157daf2261386642d22de
Imrane Arabah <59772524+arabahimrane@users.noreply.github.com>
2025-02-23 20:42:12 -0500

    add ssl

--- client/src/app/shared/service/API.ts
+++ client/src/app/shared/service/API.ts
@@ -1,6 +1,6 @@
 export enum ApiPath {
     ipApi = "https://ipapi.co/json/",
-    url = 'http://imranearabah.com/api/',
+    url = 'https://imranearabah.com/api/',
     awsFile = 'https://panadola.s3.us-west-2.amazonaws.com/',
 
     signin = url + 'signin',
