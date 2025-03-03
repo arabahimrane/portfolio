5083ada021fd2cdeb338e1a35d439b6f51e08376
Imrane Arabah <59772524+arabahimrane@users.noreply.github.com>
2025-03-03 14:35:11 -0500

    Delete serveur/config/mongodb.config.js

--- serveur/config/mongodb.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const env = require(`../environment/production`);
-
-exports.clientPromise = mongoose
-    .connect(env.dbUrl)
-    .then(() => {
-        console.log("Connected to the database successfully");
-    })
-    .catch((err) => console.error(err));
